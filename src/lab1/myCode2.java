6ad4869e56b41fb4c06c4828df640c4d88057764
boat2541 <botba109@gmail.com>
2018-06-26 15:40:26 +0700

    test @Code email

---
+++ src/lab1/myCode2.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class myCode2 {//main
+
+    //main
+    public static void main (String[] args) {
+     System.out.println("botba109@gmail.com");
+
+    }
+    //main
+
+
+}
