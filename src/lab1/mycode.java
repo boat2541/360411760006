4d68fd24a1b95f951c0a8894efcfd7a8b3fdd32e
boat2541 <botba109@gmail.com>
2018-06-26 15:06:25 +0700

    Test upload

---
+++ src/lab1/mycode.java
@@ -0,0 +1,20 @@
+package lab1;
+
+public class mycode {
+    //main
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+        System.out.println("Hell world");
+        System.out.println("Piyawat sutthipanya");
+        System.out.println("s360411760006");
+        System.out.println("MIT421");
+        System.out.println("RMUTSV");
+
+    }//main
+}
