f1a162edee96b2bba05f5c54030290c01fbfd143
BoatPiyawat <botba109@gmail.com>
2018-07-10 16:19:24 +0700

    Test git ex1.

---
+++ src/lab2/ex4.java
@@ -0,0 +1,44 @@
+package lab2;
+
+import java.util.Scanner;
+
+public class ex4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        //input
+        //name
+        System.out.print("What is your name? : ");
+        String name = scanner.nextLine();
+        //age
+        System.out.print("How own are you?: ");
+        int age = scanner.nextInt();
+        //Weight
+        System.out.print("How much do you weigth?: ");
+        double Weigth = scanner.nextDouble();
+
+        //output
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(Weigth);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+
+}//class
