f8323ea234b29294760a97de9d02eea0b272a0b9
BoatPiyawat <botba109@gmail.com>
2018-10-16 16:24:43 +0700

    Quiz3

---
+++ src/quiz3/quiz3.java
@@ -0,0 +1,33 @@
+package quiz3;
+
+import java.util.Scanner;
+
+public class quiz3 {
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    //input
+    //Fahrenheit
+    System.out.print(" Fahrenheit?: ");
+    double Fahrenheit = scanner.nextDouble();
+    //Celsius
+    double Celsius = (5.0/9.0) * (Fahrenheit -32.0);
+
+    //output
+    System.out.println("Celsius is"+Celsius);
+
+    System.out.println("Celsius is"+Celsius);
+    System.out.println("Celsius is"+Celsius);
+    System.out.println("Celsius is"+Celsius);
+
+
+
+
+
+
+
+}//main
+
+
+
+
+}//class
